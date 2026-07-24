9d8bac94b54fbe754c5a3526aebcc6230b1a6429
POOJA K K <poojakathirvelv@gmail.com>
2026-07-24 10:24:53 +0530

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int perimeter = 0;
+
+        int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    for (int[] d : dir) {
+                        int ni = i + d[0];
+                        int nj = j + d[1];
+
+                        if (ni >= 0 && ni < rows && nj >= 0 && nj < cols 
+                            && grid[ni][nj] == 1) {
+                            perimeter--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
