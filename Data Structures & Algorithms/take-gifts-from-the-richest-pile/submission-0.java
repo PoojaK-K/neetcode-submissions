e4b36eadec146c4c20d8058930e79c4a229e439a
POOJA K K <poojakathirvelv@gmail.com>
2026-07-27 10:29:15 +0530

    Add: take-gifts-from-the-richest-pile - submission-0

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int gift : gifts) {
+            pq.offer(gift);
+        }
+
+        while (k-- > 0) {
+            int max = pq.poll();
+            pq.offer((int) Math.sqrt(max));
+        }
+
+        long sum = 0;
+        while (!pq.isEmpty()) {
+            sum += pq.poll();
+        }
+
+        return sum;
+    }
+}
